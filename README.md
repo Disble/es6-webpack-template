686bc51612bb84a5c54bc63ca5b2ab61366b30cb
Disble <decoder@hotmail.es>
2021-02-21 23:23:07 -0500

    es6 webpack template
    - Load HTML template
    - Load CSS
    - Load javascript modules

---
+++ README.md
@@ -0,0 +1,9 @@
+# Instalation
+
+```
+npm install
+```
+
+```
+npm start
+```
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import path from 'path';
+
+console.log('es6-webpack-template');
+console.log(path.resolve(__dirname, 'styles'));
